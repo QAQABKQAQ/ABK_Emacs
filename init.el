5e34c47876e52c0149cd702876e78df09a8e2e68
ALBamKin <albamkin@ALBamKindeMacBook-Pro.local>
2026-01-07 20:57:42 +0800

    可以当IDE使用了

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
+
 ;; emacs的自定义设置转移到自定义文件中
 (setq custom-file "~/.config/emacs/.emacs.custom.el")
 
 (add-to-list 'load-path
-	     (expand-file-name ( concat user-emacs-directory "plugins")))  ;; ~/.config/emacs
+	     (expand-file-name ( concat user-emacs-directory "plugins")))  ;; ~/.
+
 
 (require 'init-config)
 (require 'init-package)
+
--- plugins/init-config.el
+++ plugins/init-config.el
@@ -1,3 +1,4 @@
+
 ;; 菜单栏
 (menu-bar-mode 0)
 ;; 工具栏
@@ -10,8 +11,40 @@
 (load-file custom-file)
 (ido-mode 1)
 (ido-everywhere 1)
-(defalias 'yes-or-no-p' y-or-n-p)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq inhibit-startup-screen t)
 
 (setq create-lockfiles nil)
+
+(defconst *is-mac* (eq system-type 'darwin))
+
+(when *is-mac*
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'none))
+
+
+(use-package no-littering
+  :ensure t
+  :config
+  ;; 1. 配合 recentf 清理
+  (with-eval-after-load 'recentf
+    (add-to-list 'recentf-exclude no-littering-var-directory)
+    (add-to-list 'recentf-exclude no-littering-etc-directory))
+  
+  ;; 2. 备份文件设置 (现在这里是安全的)
+  (setq backup-directory-alist
+        `(("." . ,(no-littering-expand-var-file-name "backup/"))))
+
+  ;; 3. 自动保存设置
+  (setq auto-save-file-name-transforms
+        `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
+
+(setq make-backup-files t)          ;; 依然进行备份
+(setq version-control t)            ;; 启用版本控制式备份
+(setq backup-by-copying t)          ;; 备份时复制
+(setq delete-old-versions t)        ;; 自动删除过旧的备份
+(setq kept-old-versions 2)          ;; 保留最旧的2个
+(setq kept-new-versions 5)          ;; 保留最新的5个
 
 (provide 'init-config)
