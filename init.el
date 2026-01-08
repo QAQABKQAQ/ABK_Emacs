ef51f2930871d064e085303cd536af6bef695072
ALBamKin <albamkin@ALBamKindeMacBook-Pro.local>
2026-01-08 11:11:11 +0800

    修改bug,优化启动

--- init.el
+++ init.el
@@ -4,8 +4,22 @@
 
 (add-to-list 'load-path
 	     (expand-file-name ( concat user-emacs-directory "plugins")))  ;; ~/.
+;; 1. 加速启动 (暂时调大垃圾回收阈值)
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; 2. 定义软件源 (必须在最前面！且使用 HTTP 解决卡顿)
+(require 'package)
+(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 
 
-(require 'init-config)
+(unless package-archive-contents
+  (message "Refeshing package archives...")
+  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
 (require 'init-package)
+(require 'init-config)
 
---
+++ var/transient/history.el
@@ -0,0 +1,11 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-fetch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-push nil)
+ (magit-rebase
+  ("--autostash"))
+ (magit-reset nil))
