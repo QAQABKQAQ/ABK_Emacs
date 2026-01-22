ced9e42ee7e6e044fc97c2eec0705f2de9927d69
ALBamKin <albamkin@ALBamKindeMacBook-Pro.local>
2026-01-22 13:35:49 +0800

    配置优化，增加字体，操作

--- init.el
+++ init.el
@@ -1,15 +1,60 @@
 
+;; 原生光标移动
+;; C-f/b 单个字符移动 h/l
+;; M-f/b 单个单词移动 e/b
+;; M-a/e 语句块移动
+;; M-{/} 跳过整个空行分割的块
+;; C-M-f/b 括号移动,比如从`(`跳到`)`
+;; C-M-n/p 括号跳转
+
+;; === 插件 ===
+;; C-' avy 查找字符移动，相当于nvim - flash
+;; M-g l 跳转到行
+;; M-g w 跳转到单词的首字母
+;; M-g e 把远处的行移动过来
+;; M-g c 把远处的行拷过来
+;; k -> 杀掉那一行
+;; y -> 复制那一行
+;; t -> 把那一行瞬移到我这里
+
+;; C-s 查找行字符
+;; C-x b 查找buffer
+;; M-g o `consult-outline` 大范围跳转，原理为将标题，类名，方法名作为了锚点
+;; C-c f 查找文件
+;; C-c g 查找单词
+
+;; font :
+;; Monaspace Neon medium normal
+
+
+
+
 (setq custom-file (expand-file-name ".emacs.custom.el" user-emacs-directory))
 (load custom-file t t)
 
 
 (setq inhibit-startup-message t)
 
+
+
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (sql . t)
+   (shell . t)  
+   (python . t) 
+   (js . t)     
+   ))
+
+
+(column-number-mode 0)
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-
-
+      
 (require 'package)
 (setq package-enable-at-startup nil);;取消自启
 
@@ -27,26 +72,31 @@
 (require 'cl-lib)
 (require 'use-package)
 
+
+
+
+
+;; === theme ===
 (use-package catppuccin-theme
   :ensure t
   :demand t
   :init
-  (setq catppuccin-flavor 'macchiato)
+  (setq catppuccin-flavor 'mocha)
   :config
   (load-theme 'catppuccin t)
   )
 
 
 
-(use-package multiple-cursors
+
+(use-package  multiple-cursors
   :ensure t
   :bind (
-   ("C-S-c C-S-c" . mc/edit-lines)
-   ("C->" . mc/mark-next-like-this)
-   ("C-<" . mc/mark-previous-like-this)
-   ("C-c C-<" . mc/mark-all-like-this))
+   ("C-S-c C-S-c" . mc/edit-lines);; 选中区域转换可编辑行
+   ("C->" . mc/mark-next-like-this);; 向上延伸光标
+   ("C-<" . mc/mark-previous-like-this);; 向上延伸光标
+   ("C-c C-<" . mc/mark-all-like-this));; 把当前选中的单词前面都加上光标
   )
-
 
 
 (use-package vertico
@@ -105,16 +155,79 @@
 (use-package seq
   :ensure t)
 
+
 (use-package avy
   :ensure t
   :bind
   (("C-'" . avy-goto-char-timer)
    ("M-g l" . avy-goto-line)
-   )
+   ("M-g w" . avy-goto-word-1)  ; 新增：跳到单词首字母，比 timer 更精准
+   ("M-g e" . avy-move-line)    ; 新增：把远处的行移过来
+   ("M-g c" . avy-copy-line))   ; 新增：把远处的行拷过来
   :config
   (setq avy-background t)
   (setq avy-timeout-seconds 0.5)
-)
+  (setq avy-all-windows t)      ; 允许跨窗口跳转
+  
+  ;; --- 高级功能：Dispatch (远程操作) ---
+  (defun my/avy-action-kill-whole-line (pt)
+    (save-excursion
+      (goto-char pt)
+      (kill-whole-line))
+    (select-window
+     (cdr (ring-ref avy-ring 0)))
+    t)
+
+  (defun my/avy-action-copy-whole-line (pt)
+    (save-excursion
+      (goto-char pt)
+      (cl-destructuring-bind (start . end) (bounds-of-thing-at-point 'line)
+        (copy-region-as-kill start end))
+      (message "Copied line"))
+    t)
+
+  (defun my/avy-action-yank-whole-line (pt)
+    (my/avy-action-copy-whole-line pt)
+    (save-excursion (yank))
+    t)
+
+  ;; 绑定：
+  ;; k -> 杀掉那一行
+  ;; y -> 复制那一行
+  ;; t -> 把那一行瞬移到我这里
+  (setq avy-dispatch-alist
+        '((?k . my/avy-action-kill-whole-line)
+          (?y . my/avy-action-copy-whole-line)
+          (?t . my/avy-action-yank-whole-line)))
+  )
+
+
+;; ==ctags==
+(use-package citre
+  :ensure t
+  :defer t
+  :init
+  (require 'citre-config)
+  (citre-auto-enable-citre-mode)
+  :bind
+  (
+   ("C-x c j" . citre-jump)
+   ("C-x c p" . citre-peek)
+   ("C-x c u" . citre-update-this-tags-file)
+   )
+  :config
+  (setq citre-use-project-root-when-creating-tags t)
+  (setq citre-prompt-language-for-ctags-command t )
+  (setq citre-peek-use-icons t)
+  (add-hook 'after-save-hook 
+            (lambda () 
+              (when (and (bound-and-true-p citre-mode)
+                         (citre-get-tags-file-path))
+                (citre-update-this-tags-file))))
+  
+  )
+
+
 
 
 
@@ -131,8 +244,19 @@
     :ensure t
     :hook (java-mode . eglot-java-mode)
     :init
-    (setq eglot-java-server-install-dir (expand-file-name "eclipse.jdt.ls" user-emacs-directory)))
+    (setq eglot-java-server-install-dir (expand-file-name "eclipse.jdt.ls" user-emacs-directory))
+    (setenv "JAVA_HOME" "/opt/homebrew/opt/openjdk@21/libexec/openjdk.jdk/Contents/Home")
+        (setenv "PATH" (concat "/opt/homebrew/opt/openjdk@21/libexec/openjdk.jdk/Contents/Home/bin:" (getenv "PATH")))
+  (add-to-list 'exec-path "/opt/homebrew/opt/openjdk@21/libexec/openjdk.jdk/Contents/Home/bin")
+    )
 
+(use-package nerd-icons
+	      :ensure t)
+(use-package nerd-icons-corfu
+  :ensure t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
 
 
 (use-package corfu
@@ -144,10 +268,17 @@
         ("<C-return>" . completion-at-point)) 
   (:map corfu-map
         ("S-SPC" . corfu-insert-separator))
+  :custom
+    (corfu-on-exact-match nil)
   :config
-  (setq corfu-auto nil
-        corfu-cycle t
-        ) 
+  (setq 
+   corfu-auto nil
+   corfu-cycle t
+   corfu-popupinfo-delay 0.1
+   corfu-preview-current nil
+   )
+  (corfu-popupinfo-mode)
+
 
   (unless (display-graphic-p)
     (corfu-terminal-mode +1)))
@@ -167,6 +298,7 @@
     (setq-local completion-at-point-functions
                 (list (cape-capf-super
                        #'eglot-completion-at-point ; 1. 智能代码补全 (Eglot)
+		       #'citre-completion-at-point
                        #'cape-dabbrev              ; 2. 单词补全 (Cape)
                        #'cape-file))))             ; 3. 路径补全 (Cape)
   :hook (eglot-managed-mode . my/eglot-capf))
@@ -195,11 +327,38 @@
     (interactive)
     (let ((buffer-name "*vterm*"))
       (if (equal (buffer-name) buffer-name)
-	  (switch-to-buffer (other-buffer (current-buffer)1))
+	  (switch-to-buffer (other-buffer (current-buffer) 1))
 	(if (get-buffer buffer-name)
 	    (switch-to-buffer buffer-name)
 	  (vterm buffer-name)))))
   :bind
   (("C-`" . my/toggle-vterm)))
 
+
+
+
+;; 设置字体
+;; 英文字体:https://monaspace.githubnext.com/
+;; 中文字体:https://github.com/lxgw/LxgwWenKai
+(defun my/apply-font-config ()
+  (interactive)
+  (when (display-graphic-p)
+
+    (set-face-attribute 'default nil
+			:family "Monaspace Neon"
+			:height 140
+			:weight 'normal
+			)
+    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+      (set-fontset-font t charset (font-spec :family "LXGW WenKai Mono")))
+    (setq face-font-rescale-alist '(("LXGW WenKai Mono" . 1.25))))
+  )
+  
+
+(add-hook 'after-init-hook #'my/apply-font-config)
+(add-hook 'window-setup-hook #'my/apply-font-config)
+(add-hook 'server-after-make-frame-hook #'my/apply-font-config)
+
+
+(my/apply-font-config)
 
--- transient/history.el
+++ transient/history.el
@@ -1,4 +1,7 @@
-((magit-commit nil)
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-fetch nil)
+ (magit-merge nil)
  (magit-push nil)
  (magit-remote
   ("-f")))
